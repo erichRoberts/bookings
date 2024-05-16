0fa75850f1c701f66f38e1b84ccdd182733b481b
erichRoberts <168422525+erichRoberts@users.noreply.github.com>
2024-05-16 06:54:30 +0200

    Initial Commit

---
+++ cmd/web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/erichRoberts/bookings/pkg/config"
+	"github.com/erichRoberts/bookings/pkg/handlers"
+	"github.com/erichRoberts/bookings/pkg/render"
+)
+
+// portNumber is the port that will be listened to
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	println("starting")
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = false
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+	println("created template cache")
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
+
+	println("Server listening on port ", portNumber)
+	// http.ListenAndServe(portNumber, nil)
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
